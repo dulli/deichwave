9f4cc56cb789774fd38e05876305485114015f94
Martin Dullweber <martin@dul.li>
2023-01-28 09:07:23 +0100

    feat: add a first attempt at software debouncing

--- pkg/hardware/gpio_linux.go
+++ pkg/hardware/gpio_linux.go
@@ -14,28 +14,50 @@ type GPIO struct {
 	lines []*gpiod.Line
 }
 
+// As the PCF8574 doesn't support gpiod's debouncing, we need to implement our own
+type debouncer struct {
+	last     time.Time
+	duration time.Duration
+}
+
+func (d *debouncer) active(ev gpiod.LineEvent) bool {
+	// TODO: only debounce if a falling edge follows on a rising one
+	return time.Since(d.last) < d.duration
+}
+
 func (h *GPIO) Setup(cfg common.Config) error {
 	for _, setup := range cfg.GPIO {
-		switch setup.Type {
-		case "toggles":
-			// Each input has its own pin, config object can contain multiple inputs
-			for idx, pin := range setup.Pins {
-				actions := strings.Split(setup.Actions[idx], ":")
-				err := h.setupToggle(setup.Chip, pin, setup.Debounce, actions)
+		// Closure to keep debouncers separated
+		err := func() error {
+			deb := debouncer{
+				last:     time.Now().Add(-time.Second),
+				duration: time.Duration(setup.Debounce) * time.Microsecond,
+			}
+			switch setup.Type {
+			case "toggles":
+				// Each input has its own pin, config object can contain multiple inputs
+				for idx, pin := range setup.Pins {
+					actions := strings.Split(setup.Actions[idx], ":")
+					err := h.setupToggle(setup.Chip, pin, deb, actions)
+					if err != nil {
+						return err
+					}
+				}
+			case "rotary":
+				// Each input has two to three pins, config object is a single input
+				actions := strings.Split(setup.Actions[0], ":")
+				if len(setup.Actions) > 1 {
+					actions = append(actions, strings.Split(setup.Actions[1], ":")...)
+				}
+				err := h.setupRotary(setup.Chip, setup.Pins, deb, actions)
 				if err != nil {
 					return err
 				}
 			}
-		case "rotary":
-			// Each input has two to three pins, config object is a single input
-			actions := strings.Split(setup.Actions[0], ":")
-			if len(setup.Actions) > 1 {
-				actions = append(actions, strings.Split(setup.Actions[1], ":")...)
-			}
-			err := h.setupRotary(setup.Chip, setup.Pins, setup.Debounce, actions)
-			if err != nil {
-				return err
-			}
+			return nil
+		}()
+		if err != nil {
+			return err
 		}
 	}
 	return nil
@@ -52,7 +74,7 @@ func (h *GPIO) Close() {
 }
 
 // Toggles switch between on and off states, so either buttons or switches
-func (h *GPIO) setupToggle(chip string, pin int, debounce int, actions []string) error {
+func (h *GPIO) setupToggle(chip string, pin int, deb debouncer, actions []string) error {
 	log.WithFields(log.Fields{
 		"chip":    chip,
 		"pin":     pin,
@@ -61,10 +83,33 @@ func (h *GPIO) setupToggle(chip string, pin int, debounce int, actions []string)
 	line, err := gpiod.RequestLine(chip, pin,
 		gpiod.WithPullUp,
 		gpiod.WithEventHandler(func(ev gpiod.LineEvent) {
-			log.WithFields(log.Fields{
-				"ev":     ev,
-				"driver": "gpio",
-			}).Info("Button pressed")
+			if deb.active(ev) {
+				log.WithFields(log.Fields{
+					"chip":   chip,
+					"pin":    pin,
+					"driver": "gpio",
+				}).Warn("Toggle debounced")
+				return
+			}
+			deb.last = time.Now()
+
+			switch ev.Type {
+			case gpiod.LineEventFallingEdge:
+				// action[0]
+				log.WithFields(log.Fields{
+					"chip":   chip,
+					"pin":    pin,
+					"driver": "gpio",
+				}).Info("Button pressed")
+			case gpiod.LineEventRisingEdge:
+				//action[1]
+				log.WithFields(log.Fields{
+					"chip":   chip,
+					"pin":    pin,
+					"driver": "gpio",
+				}).Info("Button released")
+			}
+
 		}),
 		gpiod.WithBothEdges)
 	h.lines = append(h.lines, line)
@@ -72,7 +117,7 @@ func (h *GPIO) setupToggle(chip string, pin int, debounce int, actions []string)
 }
 
 // Rotaries switch between left and right states and have an additional toggle when pressed
-func (h *GPIO) setupRotary(chip string, pins []int, debounce int, actions []string) error {
+func (h *GPIO) setupRotary(chip string, pins []int, deb debouncer, actions []string) error {
 	log.WithFields(log.Fields{
 		"chip":    chip,
 		"pin":     pins,
@@ -80,10 +125,22 @@ func (h *GPIO) setupRotary(chip string, pins []int, debounce int, actions []stri
 	}).Debug("Setting up rotary")
 	rotflag := false
 	line, err := gpiod.RequestLine(chip, pins[0],
+		gpiod.WithPullUp,
 		gpiod.WithEventHandler(func(ev gpiod.LineEvent) {
+			if deb.active(ev) {
+				log.WithFields(log.Fields{
+					"chip":   chip,
+					"pin":    pins,
+					"driver": "gpio",
+				}).Warn("Rotary low debounced")
+				return
+			}
+			deb.last = time.Now()
+
 			rotflag = ev.Type == gpiod.LineEventRisingEdge
 			log.WithFields(log.Fields{
-				"ev":     ev,
+				"chip":   chip,
+				"pin":    pins,
 				"flag":   rotflag,
 				"driver": "gpio",
 			}).Info("Rotary turned low")
@@ -96,21 +153,34 @@ func (h *GPIO) setupRotary(chip string, pins []int, debounce int, actions []stri
 
 	line, err = gpiod.RequestLine(chip, pins[1],
 		gpiod.WithPullUp,
-		gpiod.WithDebounce(time.Microsecond*time.Duration(debounce)),
 		gpiod.WithEventHandler(func(ev gpiod.LineEvent) {
+			if deb.active(ev) {
+				log.WithFields(log.Fields{
+					"chip":   chip,
+					"pin":    pins,
+					"driver": "gpio",
+				}).Warn("Rotary high debounced")
+				return
+			}
+			deb.last = time.Now()
+
 			if ev.Type == gpiod.LineEventFallingEdge {
 				rotflag = false
 				return
 			}
 			if rotflag {
+				// action[0]
 				log.WithFields(log.Fields{
-					"ev":     ev,
+					"chip":   chip,
+					"pin":    pins,
 					"flag":   rotflag,
 					"driver": "gpio",
 				}).Info("Rotary turned left")
 			} else {
+				// action[1]
 				log.WithFields(log.Fields{
-					"ev":     ev,
+					"chip":   chip,
+					"pin":    pins,
 					"flag":   rotflag,
 					"driver": "gpio",
 				}).Info("Rotary turned right")
@@ -124,7 +194,7 @@ func (h *GPIO) setupRotary(chip string, pins []int, debounce int, actions []stri
 
 	// Setup the toggle component
 	if len(pins) > 2 {
-		err = h.setupToggle(chip, pins[2], debounce, actions[2:3])
+		err = h.setupToggle(chip, pins[2], deb, actions[2:3])
 	}
 	return err
 }
